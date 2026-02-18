555d95e1990103e737cd0feffbf03a5d295cf6bd
Connor Neal <121360190+ConnorANeal@users.noreply.github.com>
2026-02-18 13:46:55 -0700

    Added SQLite Database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gps-database.sqlite'
+db = SQLAlchemy(app)
 
 
 app.run(debug=True)
