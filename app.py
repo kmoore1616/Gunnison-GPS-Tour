d4c857ff2d5826403eb77fbcc9e90ccf604076d5
Kyle <kyle.moore2221@gmail.com>
2026-02-18 13:42:48 -0700

    Initial flask homepage

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route("/")
+def root():
+    return render_template('home.html')
+
+app.run(debug=True)
+
