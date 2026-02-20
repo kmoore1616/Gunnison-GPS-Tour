3adfb444a9294a795e9a5f56c6d0c9a6bcf2208f
Kyle <kyle.moore2221@gmail.com>
2026-02-20 09:42:16 -0700

    Basic framework

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
+avalible_tours = ["Historic Tour", "Art Tour", "Campus Tour", "Nature Tour"]
+
 @app.route("/")
 def root():
     return render_template('home.html')
+
+@app.route("/Tours")
+def see_tours():
+    return render_template('tour_list.html', tours=avalible_tours)
+
+@app.route("/Places")
+def place():
+    return render_template('places.html')
+
+@app.route("/Tour")
+def tour():
+    return render_template("tour.html")
+
+@app.route("/Contact")
+def contact():
+    return render_template("contact.html")
 
 app.run(debug=True)
 
