287ecae004bf49c304613b5ead1002c55813a998
Connor Neal <121360190+ConnorANeal@users.noreply.github.com>
2026-03-09 15:16:05 -0600

    User tracking

--- static/index.js
+++ static/index.js
@@ -8,17 +8,20 @@
 let id;
 let target;
 let options;
+let userMarkers = [];
+let userRoutes = [];
+let userLocation;
 let testVar = 0;
 
 const mapElement = document.querySelector('gmp-map');
 
 // Borrick Hall: 38.54752661154228, -106.91834349316723
 // Front of Rady: 38.54654720973891, -106.91798247641142
-// Test Location: 38.54539051786304, -106.91779120648226
-//39.44169, 105.01355 // Denver
-//38.5452231, -106.9183068 // in Gunnison at least
 
-// test target locale
+const destinationLatLng = { lat: 38.54472787327966, lng: -106.92136913002805 }; // Gunnison Visitor Center for testing
+
+// 39.44169, -105.01355 // DENVER
+// 38.54539051786304, -106.91779120648226 // TEST LOCALE
 target = {
     latitude: 38.54539051786304,
     longitude: -106.91779120648226,
@@ -36,16 +39,16 @@ async function initMap() {
     const { Route } = await google.maps.importLibrary('routes');
     const { AdvancedMarkerElement, PinElement } = (await google.maps.importLibrary('marker'));
 
-    const destinationLatLng = { lat: 38.54472787327966, lng: -106.92136913002805 }; // Gunnison Visitor Center for testing
-
     // Setting helper functions
     function success(pos) {
         testVar = testVar + 1;
-        console.log(testVar);
-
         const crd = pos.coords;
-        console.log(crd.latitude);
-        console.log(crd.longitude);
+        console.log("Hit " + testVar + ": " + crd.latitude + ", " + crd.longitude)
+
+        if(userMarkers.length != 0) {
+            userMarkers[0].map = null;
+            userMarkers = [];
+        }
 
         //create marker at user position
         const userPin = new PinElement({
@@ -61,16 +64,38 @@ async function initMap() {
         userMarker.append(userPin);
         mapElement.append(userMarker);
 
-        /*
+        userMarkers.push(userMarker);
+
+        createRoute(crd.latitude, crd.longitude);
+
+        if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+            console.log("Congratulations, you reached the target");
+            navigator.geolocation.clearWatch(id);
+        }
+    }
+
+    function error(err) {
+        console.error(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    async function createRoute(lat, lng) {
+
+        if(userRoutes.length != 0) {
+            for(let i = 0; i < userRoutes.length; i++) {
+                userRoutes[i].setMap(null);
+            }
+            userRoutes = [];
+        }
+
         //create route
         const request = {
-            origin: crd,
+            origin: { lat: lat, lng: lng },
             destination: destinationLatLng,
             travelMode: 'WALKING',
             fields: ['path'],
         };
 
-        //await customElements.whenDefined('gmp-map');
+        await customElements.whenDefined('gmp-map');
         const map = mapElement.innerMap;
 
         const { routes } = await Route.computeRoutes(request);
@@ -83,17 +108,9 @@ async function initMap() {
             });
 
             routePath.setMap(map);
+            userRoutes.push(routePath);
         }
-        */
 
-        if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-            console.log("Congratulations, you reached the target");
-            navigator.geolocation.clearWatch(id);
-        }
-    }
-
-    function error(err) {
-        console.error(`ERROR(${err.code}): ${err.message}`);
     }
 
 
@@ -132,6 +149,19 @@ async function initMap() {
     });
     marker2.append(pin2);
     mapElement.append(marker2);
+
+
+    const pin3 = new PinElement({
+        //@ts-ignore
+        glyphText: `C`,
+        scale: 1.5,
+        background: '#FBBC04',
+    });
+    const marker3 = new AdvancedMarkerElement({
+        position: destinationLatLng,
+    });
+    marker3.append(pin3);
+    mapElement.append(marker3);
 
 
 
