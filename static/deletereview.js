e9083a19510c175eeee876bd146dc3eb91ba5a40
mold-mann <cbranstrom212@gmail.com>
2026-04-21 19:23:44 -0700

    welcome message, events, file name cleanup

--- static/deletereview.js
+++ static/deletereview.js
@@ -1,6 +1,6 @@
 function deleteReview(id) {
     if (confirm("Are you sure you want to delete this review?")) {
-        fetch(`/delete/${id}`, { method: 'POST' })
+        fetch(`/deletereview/${id}`, { method: 'POST' })
             .then(() => window.location.reload());
     }
 }
